658d059741f3ba08a344cd3279a884ba27372c68
Manuel de Almeida <ManuelAlmeida70@outlook.pt>
2026-04-23 17:13:05 +0100

    mvn clean install is running well

---
+++ anuncioSloc-server/src/main/java/pt/uan/anuncioSloc/App.java
@@ -0,0 +1,10 @@
+package pt.uan.anuncioSloc;
+
+/**
+ * Server Application
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("AnunciosLoc Server");
+    }
+}
--- anuncioSloc-server/src/test/java/pt/uan/anuncioSloc/AppTest.java
+++ anuncioSloc-server/src/test/java/pt/uan/anuncioSloc/AppTest.java
@@ -7,11 +7,9 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
  * Unit test for simple App.
  */
 public class AppTest {
-    /**
-     * Rigourous Test :-)
-     */
+
     @Test
-    public void testApp() {
+    public void shouldAnswerWithTrue() {
         assertTrue(true);
     }
 }
